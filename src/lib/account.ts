17ab3820c2c2d5e3f15e84e38c0e2e0a8c8f5563
itskhembot <itskhembot@gmail.com>
2018-10-10 19:38:53 +0800

    fix(account-mutation): refactor update balance

--- src/lib/account.ts
+++ src/lib/account.ts
@@ -1,14 +1,15 @@
 import AccountModel from '../models/account';
+import sequelize from './sequelize';
 
   export async function updateBalanceTable(obj: any, args: { request: string, account: string, amount: number }) {
-    const account = await AccountModel.findOne({ where: { id: args.account } }) as any;
     try {
-      if (account) {
-        await AccountModel.update({
-          balance: account.balance + args.amount,
-        }, { where: { id: args.account } });
-        return account.balance + args.amount;
-      }
+      const [, [account]] = await AccountModel.update({
+          balance: [[sequelize.fn('SUM', sequelize.col('balance')), args.amount]]
+        }, { where: { id: args.account } ,
+        returning: true,}) as any;
+
+        return account.balance;
+      
     } catch (err) {
       throw new Error(err.message);
     }
