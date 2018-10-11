5be6806ed6e87270d550259b4e61d55d30710a90
itskhembot <itskhembot@gmail.com>
2018-10-11 11:28:48 +0800

    fix(idempotency-unit-test): update fakes and mocks

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -1,28 +1,29 @@
 import RequestModel from '../models/request';
 
+export async function idempotency(args: any, handler: Function) {
+  const request = (await RequestModel.findOne({
+    where: { id: args.request },
+  })) as any;
+  if (request && request.result) {
+    return request.result;
+  }
 
-  export async function idempotency (args: any, handler: Function) {
-    const request = await RequestModel.findOne({ where: { id: args.request } }) as any;
-    if (request && request.result) {
-      return request.result;
-    }
+  if (request && request.error) {
+    throw new Error(request.error.message);
+  }
 
-    if (request && request.error) {
-      throw new Error(request.error.message);
-    }
-
-    let result;
-    let error;
-    try {
-      result = await handler(null, args);
-    } catch (err) {
-      error = err;
-      result = err;
-    }
-    await RequestModel.create({
-      id: args.request,
-      result,
-      error,
-    });
-    return result;
-  };
+  let result;
+  let error;
+  try {
+    result = await handler(null, args);
+  } catch (err) {
+    error = err;
+    result = err;
+  }
+  await RequestModel.create({
+    id: args.request,
+    result,
+    error,
+  });
+  return result;
+}
--- test/unit/idempotency.spec.ts
+++ test/unit/idempotency.spec.ts
@@ -2,40 +2,68 @@ import chai, { expect } from 'chai';
 import proxyquire from 'proxyquire';
 import sinon from 'sinon';
 import chaiAsPromised from 'chai-as-promised';
-import RequestModel from '../../src/models/request';
 
 chai.use(chaiAsPromised);
 
-describe('idempotency', () => {
-    describe('Given fake request', () => {
-      const fakeModel = sinon.fake(async () => {
-        return RequestModel.findOne({ where: { id: args.request } })
-      });
-      const fakeHandler = sinon.fake(async (obj: any, args: any) => {
-        return args;
-      });
-      const { default: idempotency } = proxyquire(
-        '../../../src/lib/request',
-        {
-          'request': { default: fakeModel },
-        }
-      );
-  
-      const args = {
-        request: 'd4bc436a-f43e-4461-a8bc-a91ec19ab1b4',
-        account: 'acc_8aa1eaad-36e6-487a-b6f0-3e66a6c83c2d',
-        context: 'Lorem ipsum dolor set amet.',
-        amount: 385,
-      };
-  
-      it('should call out fakes', async () => {
-        const response = await idempotency(args, fakeHandler);
-  
-        expect(fakeModel.calledOnce).to.be.true;
-        expect(fakeHandler.calledOnce).to.be.true;
-        expect(response.body).is.not.null;
-      });
+describe.only('idempotency', () => {
+  describe('Given existing request ID', () => {
+    const fakeFindOne = sinon.fake(async () => ({result:{}}));
+    const fakeModel = { findOne: fakeFindOne };
+    const fakeHandler = sinon.fake(async (obj: any, args: any) => {
+      return args;
     });
-  
-    
+    const { idempotency } = proxyquire(
+      '../../src/lib/request',
+      {
+        '../models/request': { default: fakeModel },
+      }
+    );
+
+    const args = {
+      request: 'f04826fa-9fef-428e-8f71-19c1149ee522',
+      account: 'acc_01770e44-b3eb-4351-8a2f-8f1ed45097db',
+      context: 'Animi praesentium et earum qui quo earum vero.',
+      amount: 385,
+    };
+
+    it('should call out fakes and return existing result', async () => {
+      const response = await idempotency(args, fakeHandler);
+
+      expect(fakeFindOne.calledOnce).to.be.true;
+      expect(fakeHandler.calledOnce).to.be.false;
+      expect(response.body).is.not.null;
+    });
   });
+
+  describe('Given new request ID', () => {
+    const fakeFindOne = sinon.fake(async () => null);
+    const fakeCreate = sinon.fake(async () => ({}));
+    const fakeModel = { findOne: fakeFindOne, create: fakeCreate };
+    const fakeHandler = sinon.fake(async (obj: any, args: any) => {
+      return args;
+    });
+    const { idempotency } = proxyquire(
+      '../../src/lib/request',
+      {
+        '../models/request': { default: fakeModel },
+      }
+    );
+
+    const args = {
+      request: 'f04826fa-9fef-428e-8f71-19c1149ee522',
+      account: 'acc_01770e44-b3eb-4351-8a2f-8f1ed45097db',
+      context: 'Animi praesentium et earum qui quo earum vero.',
+      amount: 385,
+    };
+
+    it('should call handler', async () => {
+      const response = await idempotency(args, fakeHandler);
+
+      expect(fakeFindOne.calledOnce).to.be.true;
+      expect(fakeHandler.calledOnce).to.be.true;
+      expect(fakeCreate.calledOnce).to.be.true;
+      expect(fakeCreate.lastCall.lastArg).to.have.property('id', args.request);
+      expect(response.body).is.not.null;
+    });
+  });
+});
