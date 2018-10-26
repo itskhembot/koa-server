f7b7fa53b07d6de6bd081eec61ea6d8517926f4c
itskhembot <itskhembot@gmail.com>
2018-10-26 09:41:59 +0800

    feat(reserved-balance-aggregates): add reserved balance in mutations

--- src/resolvers/mutations.ts
+++ src/resolvers/mutations.ts
@@ -1,11 +1,10 @@
 import AggregateFactory from '../lib/aggregate-factory';
-import { AccountBalanceAggregate } from '../../src/aggregates';
-import { idempotency } from '../lib/request';
 import {
-  createReserved,
-  updateReserved,
-  releaseReserved,
-} from '../lib/reserved-balance';
+  AccountBalanceAggregate,
+  ReservedBalanceAggregate,
+} from '../../src/aggregates';
+import { idempotency } from '../lib/request';
+
 import {
   createVirtual,
   updateVirtual,
@@ -17,19 +16,100 @@ export default {
   updateBalance: async (
     obj: any,
     args: { request: string; account: string; amount: number }
-  ) => idempotency(args, updateBalanceTable),
+  ) =>
+    idempotency(args, async function(
+      obj: any,
+      args: { request: string; account: string; amount: number }
+    ) {
+      const aggregate = (await AggregateFactory.findOrCreateAggregate(
+        AccountBalanceAggregate,
+        args.account
+      )) as AccountBalanceAggregate;
+      aggregate.fold();
+      aggregate.updateBalance(args.amount);
+      return aggregate.state.balance;
+    }),
   createReservedBalance: async (
     obj: any,
     args: { request: string; account: string; context: string; amount: number }
-  ) => idempotency(args, createReserved),
+  ) =>
+    idempotency(args, async function(
+      obj: any,
+      args: {
+        request: string;
+        account: string;
+        context: string;
+        amount: number;
+      }
+    ) {
+      const aggregate = (await AggregateFactory.findOrCreateAggregate(
+        ReservedBalanceAggregate,
+        args.account
+      )) as ReservedBalanceAggregate;
+      aggregate.fold();
+      aggregate.create(args.account, args.context, args.amount);
+      if (aggregate.state) {
+        return {
+          id: aggregate.id,
+          account: args.account,
+          context: args.context,
+          balance: aggregate.state.balance,
+          isReleased: aggregate.state.isReleased,
+        };
+      }
+    }),
   updateReservedBalance: async (
     obj: any,
     args: { request: string; account: string; context: string; amount: number }
-  ) => idempotency(args, updateReserved),
+  ) =>
+    idempotency(args, async function(
+      obj: any,
+      args: {
+        request: string;
+        account: string;
+        context: string;
+        amount: number;
+      }
+    ) {
+      const aggregate = (await AggregateFactory.findOrCreateAggregate(
+        ReservedBalanceAggregate,
+        args.account
+      )) as ReservedBalanceAggregate;
+      aggregate.fold();
+      aggregate.update(args.account, args.context, args.amount);
+      if (aggregate.state) {
+        return {
+          id: aggregate.id,
+          account: args.account,
+          context: args.context,
+          balance: aggregate.state.balance,
+          isReleased: aggregate.state.isReleased,
+        };
+      }
+    }),
   releaseReservedBalance: async (
     obj: any,
     args: { request: string; account: string; context: string }
-  ) => idempotency(args, releaseReserved),
+  ) =>
+    idempotency(args, async function(
+      obj: any,
+      args: {
+        request: string;
+        account: string;
+        context: string;
+        amount: number;
+      }
+    ) {
+      const aggregate = (await AggregateFactory.findOrCreateAggregate(
+        ReservedBalanceAggregate,
+        args.account
+      )) as ReservedBalanceAggregate;
+      aggregate.fold();
+      aggregate.release(args.account, args.context);
+      if (aggregate.state) {
+        return aggregate.state.isReleased;
+      }
+    }),
   createVirtualBalance: async (
     obj: any,
     args: { request: string; account: string; context: string; amount: number }
@@ -47,16 +127,3 @@ export default {
     args: { request: string; account: string; context: string }
   ) => idempotency(args, commitVirtual),
 };
-
-async function updateBalanceTable(
-  obj: any,
-  args: { request: string; account: string; amount: number }
-) {
-  const aggregate = (await AggregateFactory.findOrCreateAggregate(
-    AccountBalanceAggregate,
-    args.account
-  )) as AccountBalanceAggregate;
-  aggregate.fold();
-  aggregate.updateBalance(args.amount);
-  return aggregate.state.balance;
-}
